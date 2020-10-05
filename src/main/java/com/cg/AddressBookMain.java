d09186c53d79c14ff7e4e7ff275cf6c91fdcccba
krove <krove@DESKTOP-Q5SED1R>
2020-10-06 01:34:29 +0530

    [Kashif]UC2 added. Taking details from user

--- src/main/java/com/cg/AddressBookMain.java
+++ src/main/java/com/cg/AddressBookMain.java
@@ -3,9 +3,54 @@
  */
 package com.cg;
 
+import java.util.*;
+
 public class AddressBookMain {
 	public static void main(String[] args) {
-		System.out.println("Welcome");
-		Contact contact=new Contact();
-    }
+		System.out.println("Welcome");	
+		
+		Scanner in = new Scanner(System.in);
+		boolean b = false;
+		Contact contact = new Contact();
+		System.out.print("Enter first name- ");
+		while(!b) {
+		b = contact.setFirstName(in.next());
+		}
+		b = false;
+		System.out.print("Enter last name- ");
+		while(!b) {
+		b = contact.setLastName(in.next());
+		}
+		b = false;
+		System.out.print("Enter address- ");
+		while(!b) {
+		b = contact.setAddress(in.next());
+		}
+		b = false;
+		System.out.print("Enter city- ");
+		while(!b) {
+		b = contact.setCity(in.next());
+		}
+		b = false;
+		System.out.print("Enter state- ");
+		while(!b) {
+		b = contact.setState(in.next());
+		}
+		b = false;
+		System.out.print("Enter zip- ");
+		while(!b) {
+		b = contact.setZip(in.next());
+		}
+		b = false;
+		System.out.print("Enter phone number- ");
+		while(!b) {
+		b = contact.setPhoneNo(in.next());
+		}
+		b = false;
+		System.out.print("Enter email- ");
+		while(!b) {
+		b = contact.setEmail(in.next());
+		}
+		System.out.println("New contact added !");
+	} 
 }
