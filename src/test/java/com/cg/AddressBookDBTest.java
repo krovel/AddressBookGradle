88b9ce3d7540dae0b665ce8961037fa6952cf6a8
krove <krove@DESKTOP-Q5SED1R>
2020-11-06 12:40:33 +0530

    [Kashif]Added:Method to update contact info in DB and sync it

--- src/test/java/com/cg/AddressBookDBTest.java
+++ src/test/java/com/cg/AddressBookDBTest.java
@@ -2,23 +2,24 @@ package com.cg;
 
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
+
+import com.cg.AddressbookService.IOService;
 
 import org.junit.Assert;
 
 public class AddressBookDBTest {
-
-	private AddressBookDBService addressBookDBService;
-
-	@Before
-	public void initialize() {
-		addressBookDBService = new AddressBookDBService();
-	}
-
+	
 	@Test
-	public void givenAddressBookDB_ShouldMatchCount() throws AddressBookDBException {
-		List<Contact> contactList = addressBookDBService.readContacts();
-		Assert.assertEquals(4, contactList.size());
+	public void givenAddressBookInDB_WhenRetrived_ShouldMatchContactCount() {
+		AddressbookService addressBookService = new AddressbookService();
+		List<Contact> addressBookData = addressBookService.readAddressBookData(IOService.DB_IO);
+		Assert.assertEquals(4, addressBookData.size());
+	}
+	public void givenEmployeePayrollInDB_WhenUpdated_ShouldBeInSyncWithDB() {
+		AddressbookService addressBookService = new AddressbookService();
+		addressBookService.updateAddressBookData("Kashif",950659);
+		boolean result = addressBookService.checkAddressbookInSyncWithDB("Kashif");
+		Assert.assertTrue(result);
 	}
 }
